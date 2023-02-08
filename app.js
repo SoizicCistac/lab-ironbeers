3bfc107974cdb924342162889d188de1b6c43588
Soizic Cistac <soizic.cistac@gmail.com>
2023-02-08 18:02:07 +0100

    done (almost... iteration 7 in progress

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+
 // Register the location for handlebars partials here:
 
 // ...
@@ -21,5 +23,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  const beersArray = await punkAPI.getBeers()
+  // console.log(beersArray)
+  res.render('beers', {
+    beers : beersArray
+  })
+})
+
+app.get('/random-beers', async (req, res) => {
+  const randomBeer = await punkAPI.getRandom()
+  // console.log(randomBeer)
+  res.render('random-beers', {
+    beer: randomBeer[0]
+  })
+})
+
+app.get('/beers/beer:id', async (req, res) => {
+  const beerId = await punkAPI.getBeer(req.params.id)
+  // console.log(beerId)
+  res.render('beer', {
+    beer: beerId
+  })
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
